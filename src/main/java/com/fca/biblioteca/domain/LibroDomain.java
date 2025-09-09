83dcfcd71c30ceb1b40e8aa5f28ad9f4b09ea8f1
hector <hectorsilva0423@gmai.com>
2025-09-09 10:28:51 -0600

    Aplicacion de streams y predicados

--- src/main/java/com/fca/biblioteca/domain/LibroDomain.java
+++ src/main/java/com/fca/biblioteca/domain/LibroDomain.java
@@ -5,7 +5,9 @@ import com.fca.biblioteca.data.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,10 +16,17 @@ public class LibroDomain {
     private LibroRepository libroRepository;
 
     public List<Libro> buscarLibroPorTitulo(String titulo,  String edicion){
+
+        if (titulo == null || edicion == null || titulo.isEmpty() || edicion.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        Predicate<Libro> filtroLibro = libro -> libro !=null && libro.getTitulo().equals(titulo)
+                && libro.getEdicion().equals(edicion);
+       Predicate<Libro> filtroDisponible = disponible -> disponible !=null && disponible.getExistencia()>0;
         return libroRepository.findAll()
                 .stream()
-                .filter(libro -> libro.getTitulo().equals(titulo))
-                .filter(libro -> libro.getEdicion().equals(edicion))
+                .filter(filtroLibro.and(filtroDisponible))
                 .collect(Collectors.toList());
         //return  libroRepository.findByTituloContainingIgnoreCase(titulo);
     }
